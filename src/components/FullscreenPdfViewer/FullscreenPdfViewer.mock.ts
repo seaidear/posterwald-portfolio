5d628badc0b9cfd00fcf7fd3a0aca5199d85dbce
seaidear <seaidear323@gmail.com>
2025-03-11 09:11:56 -0700

    fixed: resume profile

--- src/components/FullscreenPdfViewer/FullscreenPdfViewer.mock.ts
+++ src/components/FullscreenPdfViewer/FullscreenPdfViewer.mock.ts
@@ -2,7 +2,7 @@
 import { FullscreenPdfViewerProps } from "./FullscreenPdfViewer";
 
 const mockData: FullscreenPdfViewerProps = {
-  pdfFile: { url: "https://www.datocms-assets.com/60761/1643059325-cv_en-1.pdf" },
+  pdfFile: { url: "" },
   pdfTitle: "Curriculum Vitae",
 };
 
